1ae0a0efc9281d64bc8167e1c3ceb27ad8e0ad06
Melvi Ts <layzerar@gmail.com>
2016-01-16 16:33:30 +0800

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gospel",
-    version="0.1.4",
+    version="0.1.5",
     description="Utilities for deploying applications",
     author="Melvi Ts",
     author_email="layzerar@gmail.com",
